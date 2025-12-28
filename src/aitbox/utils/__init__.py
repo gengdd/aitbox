e6114756949d42f778ea193139ee57cd1a6c0de8
gengdd <1070496016@qq.com>
2025-12-28 18:38:49 +0800

    [ADD] add log class

---
+++ src/aitbox/utils/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File        : __init__.py
+Project     : aitbox
+Author      : gdd
+Created     : 2025-12-28
+Description :
+"""
---
+++ src/aitbox/utils/log.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File        : log.py
+Project     : aitbox
+Author      : gdd
+Created     : 2025-12-28
+Description :
+"""
+
+import sys
+import loguru
+
+
+class Log:
+    """ """
+
+    logger = loguru.logger
+    format = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+        "<level>{level: <8}</level> | "
+       "<cyan>{file}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+        "<level>{message}</level>"
+    )
+    level = "INFO"
+    logger.remove()
+    logger.add(sys.stdout, format=format, level=level)
+    
+    @classmethod
+    def _log(cls, level, message, *args, depth=2, **kwargs):
+        """ """
+        cls.logger.opt(depth=depth).log(level, message, *args, **kwargs)
+
+    @classmethod
+    def info(cls, message, *args, **kwargs):
+        """ """
+        cls._log("INFO", message, *args, **kwargs)
+
+    @classmethod
+    def warning(cls, message, *args, **kwargs):
+        """ """
+        cls._log("WARNING", message, *args, **kwargs)
+
+    @classmethod
+    def error(cls, message, *args, **kwargs):
+        """ """
+        cls._log("ERROR", message, *args, **kwargs)
+
+    @classmethod
+    def debug(cls, message, *args, **kwargs):
+        """ """
+        cls._log("DEBUG", message, *args, **kwargs)
+
+    @classmethod
+    def exception(cls, message, *args, **kwargs):
+        """ """
+        cls.logger.opt(depth=2).exception(message, *args, **kwargs)
+
+    @classmethod
+    def remove(cls):
+        """ """
+        cls.logger.remove()
+
+    @classmethod
+    def add_filename(
+        cls,
+        filename: str,
+        level: str = "INFO",
+        rotation: str = "10 MB",
+        retention: str = "7 days",
+        compression: str = "zip",
+        enqueue: bool = True,
+    ):
+        """ """
+        return cls.logger.add(
+            filename,
+            level=level,
+            format=cls.format,
+            rotation=rotation,
+            retention=retention,
+            compression=compression,
+            enqueue=enqueue,
+            colorize=False,
+        )
+
+
+class Logger:
+    """ """
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
